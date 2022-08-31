002b0948ef66058ddb120e33ed956c8fc72912c3
yschristian <yubahwesc@gmail.com>
2022-08-31 18:05:06 +0200

    first

---
+++ src/data.js
@@ -0,0 +1,86 @@
+export const sliderItems = [
+    {
+        id:1,
+        img:"https://www.pngmart.com/files/17/Girl-Long-Dress-Transparent-Background.png",
+        title:"DATING DRESS SALE",
+        desc:"DON'T COMPRIMISE ON STYLE GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg:"f5fafd"
+    },
+    {
+        id:2,
+        img:"https://pngimg.com/uploads/dress/dress_PNG40.png",
+        title:"POPULAR DRESS SALE",
+        desc:"DON'T COMPRIMISE ON STYLE GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg:"fcf1ed"
+    },
+    {
+        id:3,
+        img:"https://www.pngkit.com/png/full/86-865680_red-dress-png-image-background-red-dress-transparent.png",
+        title:"PARTY DRESS SALE",
+        desc:"DON'T COMPRIMISE ON STYLE GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg:"fbf0f4"
+    }
+]
+
+export const categories = [
+    {
+        id:1,
+        img:"https://images-eu.ssl-images-amazon.com/images/I/71M6VcybJML._AC._SR360,460.jpg",
+        title:"SHIRT STYLE"
+    },
+    {
+        id:2,
+        img:"https://www.fodors.com/wp-content/uploads/2020/09/Loungewear__HERO__HERO-970SAG-10.jpg",
+        title:"LOUNGEWEAR STYLE"
+    },
+    {
+        id:3,
+        img:"https://media-cldnry.s-nbcnews.com/image/upload/newscms/2022_13/1858600/3046075712-1-1-1-6242ff0a7e1fb.jpg",
+        title:"LIGHT JACKETS"
+    }
+]
+
+export const popularProducts =[
+    {
+        id:1,
+        img:"https://www.tezenis.com/dw/image/v2/BCXQ_PRD/on/demandware.static/-/Sites-TEZ_EC_COM/default/dw52cd15a2/images/2MM15C9120-F.jpg?sw=600&sfrm=jpeg"
+    },
+    {
+        id:2,
+        img:"https://img.sonofatailor.com/images/customizer/product/White_O_Crew_Regular_NoPocket.jpg"
+    },
+    {
+        id:3,
+        img:"https://3.imimg.com/data3/JV/LD/GLADMIN-198/ladies-shirts-250x250.jpg"
+    },
+    {
+        id:4,
+        img:"https://www.forever21.com/dw/image/v2/BFKH_PRD/on/demandware.static/-/Sites-f21-master-catalog/default/dwf2f557e2/1_front_750/00467810-08.jpg"
+    },
+    {
+        id:5,
+        img:"https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1617793374-bestspringjacket-whistles-1617792822.jpg"
+    },
+    {
+        id:6,
+        img:"https://pyxis.nymag.com/v1/imgs/1c6/841/5c709b37d80b170dab635914cdbdc9d683.jpg"
+    },
+    {
+        id:7,
+        img:"https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1608759026-uniqlo-1608759020.jpg"
+    },
+    {
+        id:8,
+        img:"https://images.squarespace-cdn.com/content/v1/559c4026e4b0b05c516049a2/1587737721963-JONUYV45DJCLQ6DYFI54/Womens+Loungewear+5.jpg"
+    },
+    {
+        id:9,
+        img:"https://m.media-amazon.com/images/I/41VATa9ZjhL.jpg"
+    },
+    {
+        id:10,
+        img:"https://m.media-amazon.com/images/I/61rQv1YqvjL._UL1333_.jpg"
+    }
+
+
+]
