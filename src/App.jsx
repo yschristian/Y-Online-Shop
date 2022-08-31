ec20dc026fb81d7f57a0e3807fac80d88217f791
yschristian <yubahwesc@gmail.com>
2022-08-31 18:12:12 +0200

    finally project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Product from './components/Product'
+import Products from './components/Products'
+import Cart from './pages/Cart'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import ProductList from './pages/ProductList'
+import Register from './pages/Register'
+
+const App = () =>{
+  return (
+    <Home/>
+  )
+}
+
+export default App
