adb0bd4dfcb86b9114723b25ea5b649d9bdcd6e0
yschristian <yubahwesc@gmail.com>
2022-09-08 16:40:17 +0200

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import Product from './components/Product'
+import Product from './pages/Product'
 import Products from './components/Products'
 import Cart from './pages/Cart'
 import Home from './pages/Home'
+import {BrowserRouter as Router , Routes ,Route, } from "react-router-dom";
 import Login from './pages/Login'
 import ProductList from './pages/ProductList'
 import Register from './pages/Register'
 
 const App = () =>{
+  const user = true
   return (
-    <Home/>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/products' element={<ProductList/>}/>
+        <Route path='/products/:category' element={<ProductList/>}/>
+        <Route path='/product/:id' element={<Product/>} />
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
+    </Router>
   )
 }
 
