742dccf3ed2f2f62ff04a3881b9f02ae3a7633cf
yschristian <yubahwesc@gmail.com>
2022-09-08 16:38:19 +0200

    products of web

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,3 +1,5 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 import styled from "styled-components"
 import {popularProducts} from "../data"
 import Product from "./Product"
@@ -9,12 +11,60 @@ flex-wrap: wrap;
 justify-content: space-between;
 `
 
-const Products = () => {
+const Products = ({cat, filters,sort}) => {
+  const [products , setProducts] = useState([])
+  const [filteredProducts , setFilteredProducts] = useState([])
+
+  useEffect(()=>{
+    const getProducts = async ()=>{
+      try {
+        const res = await axios.get(
+           cat ? `http://localhost:5000/api/product/getAllProduct?category=${cat}`
+              : "http://localhost:5000/api/product/getAllProduct"
+        )
+      console.log(res);
+        setProducts(res.data)
+      } catch (error) {
+        
+      }
+    }
+    getProducts()
+  },[cat]);
+ 
+  useEffect(()=>{
+    cat && setFilteredProducts(
+      products.filter((item)=>
+      Object.entries(filters).every(([key, value])=>
+        item[key].includes(value)
+      )
+      )
+    )
+  },[products,cat,filters])
+
+  useEffect(()=>{
+    if((sort==="newest")){
+      setFilteredProducts((prev)=>
+        [...prev].sort((a,b)=> a.createdAt - b.createdAt)
+      )
+    }else  if((sort==="asc")){
+      setFilteredProducts((prev)=>
+        [...prev].sort((a,b)=> a.price - b.price)
+      )
+    }else{
+      setFilteredProducts((prev)=>
+        [...prev].sort((a,b)=> b.price - a.price)
+      )
+    }
+  },[sort])
+
   return (
     <Container>
-      {popularProducts.map((item)=>(
+      {cat ? filteredProducts.map((item)=>(
          <Product item ={item} key={item.id}/>
-      ))}
+      )) : products.slice(0,8).map((item)=>(
+        <Product item ={item} key={item.id}/>
+     ))
+    }
     </Container>
   )
 }
