ac3cc4960c2e18f2a2834153dafb6e2f5cf6c1fe
yschristian <yubahwesc@gmail.com>
2022-09-08 16:36:19 +0200

    categoryItem

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import styled from 'styled-components'
 import { mobile } from '../Responsive';
 
@@ -48,11 +49,13 @@ const Button =  styled.button`
 const CategoryItem = ({item}) => {
   return (
     <Container>
+      <Link to={`/products/${item.cat}`}>
          <Image src={item.img} />
             <Info>
                  <Title>{item.title}</Title>
                  <Button>SHOP NOW</Button>
             </Info>
+      </Link>
     </Container>
   )
 }
