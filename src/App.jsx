d1be83e8008e0a098af2c2c3693bd24fd3219087
yschristian <yubahwesc@gmail.com>
2022-09-12 14:35:56 +0200

    user logged in and redirected

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,15 @@ import Products from './components/Products'
 import Cart from './pages/Cart'
 import Home from './pages/Home'
 import "./App.css"
-import {BrowserRouter as Router , Routes ,Route, } from "react-router-dom";
+import {BrowserRouter as Router , Routes ,Route, Navigate, } from "react-router-dom";
 import Login from './pages/Login'
 import ProductList from './pages/ProductList'
 import Register from './pages/Register'
 import Success from './pages/Success'
+import { useSelector } from 'react-redux'
 
 const App = () =>{
-  const user = true
+  const user = useSelector((state)=> state.user.currentUser)
   return (
     <Router>
       <Routes>
@@ -22,7 +23,10 @@ const App = () =>{
         <Route path='/product/:id' element={<Product/>} />
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/success' element={<Success/>}/>
-        <Route path='/login' element={<Login/>} />
+         <Route 
+         path='/login'
+         element={ user ? ( <Navigate replace to="/"/>) : (<Login/>)} 
+         />
         <Route path='/register' element={<Register/>}/>
       </Routes>
     </Router>
